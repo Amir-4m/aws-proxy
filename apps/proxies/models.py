438ec3579d7b36cdbc1c50ce0eb4133ee63d93cf
Amir <amiri.amir30@yahoo.com>
2021-01-10 00:05:32 +0330

    Proxy model updated

--- apps/proxies/models.py
+++ apps/proxies/models.py
@@ -8,15 +8,6 @@ from apps.servers.models import Server
 
 
 class Proxy(models.Model):
-    STATUS_CHECK = 'check'
-    STATUS_ACTIVE = 'active'
-    STATUS_DEACTIVATED = 'deactivated'
-
-    STATUS_CHOICES = [
-        (STATUS_CHECK, _('Check')),
-        (STATUS_ACTIVE, _('Active')),
-        (STATUS_DEACTIVATED, _('Deactivated')),
-    ]
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
     hash_key = models.UUIDField(default=uuid.uuid4)
@@ -24,7 +15,6 @@ class Proxy(models.Model):
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='proxies', null=True)
     host = models.CharField(_('host'), max_length=32, null=True, blank=True)
     port = models.IntegerField(_('port'))
-    status = models.CharField(_('status'), max_length=11, choices=STATUS_CHOICES, default=STATUS_DEACTIVATED)
     secret_key = models.CharField(max_length=32)
     is_enable = models.BooleanField(default=True)
 
