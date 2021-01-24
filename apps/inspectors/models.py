f3ed221e66916b95698089c4bf382e39598c5113
amir <a.amiri@hami.blue>
2021-01-24 18:14:52 +0330

    isp models added

--- apps/inspectors/models.py
+++ apps/inspectors/models.py
@@ -7,20 +7,36 @@ from apps.inspectors.utils.jwt import jwt_payload_handler, jwt_encode_handler
 from apps.servers.models import Server
 
 
-class Inspector(models.Model):
-    OPERATOR_TCI = 'tci'
-    OPERATOR_MCI = 'mci'
-    OPERATOR_MTN = 'mtn'
+class ISPDetector(models.Model):
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+    title = models.CharField(_('ISP title'), max_length=40)
+    # proxy_type = models.PositiveSmallIntegerField(_('proxy type'), choices=TelProxy.PROXY_TYPES)
+    regex_pattern = models.TextField(_('regex pattern'))
+    is_enable = models.BooleanField(_('enabled?'), default=True)
 
-    OPERATOR_CHOICES = [
-        (OPERATOR_TCI, _('TCI')),
-        (OPERATOR_MCI, _('MCI')),
-        (OPERATOR_MTN, _('MTN')),
-    ]
+    class Meta:
+        verbose_name = _('ISP detector')
+        verbose_name_plural = _('ISP detectors')
+
+    def __str__(self):
+        return self.title
+
+
+class Inspector(models.Model):
+    # OPERATOR_TCI = 'tci'
+    # OPERATOR_MCI = 'mci'
+    # OPERATOR_MTN = 'mtn'
+    #
+    # OPERATOR_CHOICES = [
+    #     (OPERATOR_TCI, _('TCI')),
+    #     (OPERATOR_MCI, _('MCI')),
+    #     (OPERATOR_MTN, _('MTN')),
+    # ]
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
     name = models.CharField(_('name'), max_length=120)
-    operator = models.CharField(_('operator'), max_length=3, choices=OPERATOR_CHOICES, default=OPERATOR_TCI)
+    # operator = models.CharField(_('operator'), max_length=3, choices=OPERATOR_CHOICES, default=OPERATOR_TCI)
     is_enable = models.BooleanField(_('enabled?'), default=True)
 
     def __str__(self):
@@ -41,6 +57,7 @@ class InspectorLog(models.Model):
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     inspector = models.ForeignKey(Inspector, on_delete=models.CASCADE, related_name='inspector_logs')
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='inspector_logs')
+    ip = models.CharField(_('ip'), max_length=15, null=True)
     hash_key = models.UUIDField(_('hash key'))
     is_active = models.BooleanField(_('active?'))
 
