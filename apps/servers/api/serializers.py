702bbdb1dcbbb10cdb0b205bae05f73c55dbe8e3
amir <a.amiri@hami.blue>
2021-01-25 17:39:34 +0330

    server serializer updated

--- apps/servers/api/serializers.py
+++ apps/servers/api/serializers.py
@@ -5,7 +5,8 @@ from ..models import Server
 
 class ServerSerializer(serializers.ModelSerializer):
     ip = serializers.CharField(source='active_ip')
+    port = serializers.CharField(default='80')
 
     class Meta:
         model = Server
-        fields = ('id', 'hash_key', 'ip', 'connection_status')
+        fields = ('id', 'hash_key', 'ip', 'port', 'connection_status')
