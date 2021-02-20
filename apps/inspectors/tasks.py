364572024abd022685e930b177d06b83e5f54772
Amir <amiri.amir30@yahoo.com>
2021-02-20 14:57:22 +0330

    inspector analyses task updated

--- apps/inspectors/tasks.py
+++ apps/inspectors/tasks.py
@@ -19,13 +19,11 @@ logger = logging.getLogger(__name__)
 def check_server_connection_analyses():
     with transaction.atomic():
         server_list = Server.objects.select_for_update().filter(connection_status=Server.CONNECTION_STATUS_CHECK, is_enable=True)
-
-        """
         isp_list = list(ISPDetector.objects.filter(is_enable=True))
         for server in server_list:
             _restart = []
             for isp in isp_list:
-                _inspectors = InspectorLog.objects.filter(detected_isp=isp)
+                _inspectors = InspectorLog.objects.filter(detected_isp=isp, inspector__is_approved=True)
                 inspected_servers = _inspectors.filter(hash_key=server.hash_key)
                 inspector_count = _inspectors.filter(
                     ip=server.active_ip()
@@ -38,17 +36,15 @@ def check_server_connection_analyses():
                     _restart.append(True)
                 else:
                     _restart.append(None)
-        """
 
-        for server in server_list:
-            inspected_servers = InspectorLog.objects.filter(hash_key=server.hash_key)
-            if inspected_servers.filter(is_active=False).exists():
+            if all([r is False for r in _restart]):
+                server.connection_status = Server.CONNECTION_STATUS_ACTIVE
+                server.save()
+            elif any(_restart):
                 restart_server.delay(server.id)
             elif (timezone.now() - server.updated_time).total_seconds() > settings.SERVER_EXPIRY_TIME * 60:
                 restart_server.delay(server.id)
-            elif inspected_servers.count() >= 3:
-                server.connection_status = Server.CONNECTION_STATUS_ACTIVE
-                server.save()
+
 
 @periodic_task(run_every=(crontab(**settings.CLEAR_INSPECTOR_LOGS_CRONTAB)))
 def clear_inspector_logs():
